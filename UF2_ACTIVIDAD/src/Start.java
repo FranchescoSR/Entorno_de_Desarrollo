3441fd4c4bf609530b93179278b745bd0a69baf0
Franchesco <franchesco127@hotmail.com>
2022-11-06 00:22:28 +0100

    2 AVANCE Refactorizado

--- UF2_ACTIVIDAD/src/Start.java
+++ UF2_ACTIVIDAD/src/Start.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class Start {
 
+	private static int hora;
+	private static int minutos;
+	private static int segundos;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner entradaDatos = new Scanner(System.in);
-		int hora = 0;
-		int minutos = 0;
-		int segundos = 0;
+		hora = 0;
+		minutos = 0;
+		segundos = 0;
 		
 		System.out.println("****************");
 		System.out.println("***RETO 1 UF1***");
@@ -20,6 +24,11 @@ public class Start {
         System.out.print("Introduzca segundos: ");
         segundos =  entradaDatos.nextInt();
 		
+		verificarHora(hora, minutos, segundos);
+	}
+
+	private static void verificarHora(int hora, int minutos, int segundos) 
+	{
 		if (hora >= 0 && hora < 24 && minutos >= 0 && minutos <= 60 && segundos >= 0 && segundos <= 60) 
 		{
 			System.out.println("Hora correcta");
